426bad8d1ad8b04ca9d6ef2ec46532063b781934
Theo Steiner <theosteiner@gmail.com>
2022-04-03 17:53:02 +0900

    Cleanup code structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,66 +1,63 @@
 <script>
-	import { onDestroy, onMount } from 'svelte';
-	import { browser } from '$app/env';
+	import { onMount } from 'svelte';
+	import { manipulateInputs } from '$lib/contentScript';
+	import { getCurrentTab } from '$lib/utils';
 
-	async function getCurrentTab() {
-		let queryOptions = { active: true, currentWindow: true };
-		let [tab] = await chrome.tabs.query(queryOptions);
-		return tab;
-	}
+	let interaction;
 
-	function injectedFunction() {
-		document.body.style.backgroundColor = 'teal';
-		chrome.runtime.sendMessage({ greeting: 'injected function was executed' }, (response) => {
-			console.log('receivedResponse', response);
-		});
-	}
-
-	let details;
-	let resumeExecution;
-	let abortExecution;
-
-	const handleClick = async (_) => {
+	const injectContentScript = async (_) => {
 		const { id: tabId } = await getCurrentTab();
 		chrome.scripting.executeScript({
 			target: { tabId },
-			function: injectedFunction
+			function: manipulateInputs
 		});
 	};
 
-	const handleMessage = (message, _sender, sendResponse) => {
+	const handleContentScriptMessage = (message, _sender, sendResponse) => {
 		waitForUserInteraction(message).then(sendResponse);
 		return true;
 	};
 
 	async function waitForUserInteraction(message) {
-		details = message.greeting;
+		const newInteraction = { data: message.greeting };
 		try {
 			return await new Promise((resolve, reject) => {
-				resumeExecution = resolve;
-				abortExecution = reject;
+				newInteraction.resumeExecution = resolve;
+				newInteraction.abortExecution = reject;
+				interaction = newInteraction;
 			});
 		} catch (abortMessage) {
 			return abortMessage;
 		}
 	}
 
+	// Add listener for messages sent from the content script
 	onMount(() => {
-		chrome.runtime.onMessage.addListener(handleMessage);
-	});
-	onDestroy(() => {
-		if (browser) {
-			chrome.runtime.onMessage.removeListener(handleMessage);
-		}
+		chrome.runtime.onMessage.addListener(handleContentScriptMessage);
+		// cleanup on destroy
+		return () => {
+			chrome.runtime.onMessage.removeListener(handleContentScriptMessage);
+		};
 	});
 </script>
 
-{#if details && resumeExecution && abortExecution}
+{#if interaction}
 	<p>
-		{details}
+		{interaction.data}
 	</p>
-	<button on:click={() => resumeExecution(`Resumed execution`)}>go ahead</button>
-	<button on:click={() => abortExecution(`Aborted execution`)}>abort</button>
+	<button
+		on:click={() => {
+			interaction.resumeExecution(`Resumed execution`);
+			interaction = null;
+		}}>go ahead</button
+	>
+	<button
+		on:click={() => {
+			interaction.abortExecution(`Aborted execution`);
+			interaction = null;
+		}}>abort</button
+	>
 {:else}
 	<p>Ready when you are!</p>
-	<button on:click={handleClick}>Execute script</button>
+	<button on:click={injectContentScript}>Execute script</button>
 {/if}
